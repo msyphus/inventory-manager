6463d9fd58f8d971cb4ec432549a0a5cab696fc2
msyphus <mark.syphus@gmail.com>
2020-05-09 21:51:52 -0600

    created and stylized function for printing a table of all inventory in stock

--- customer.js
+++ customer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
+var colors = require("colors");
 var sqlpass = require("./assets/key");
 
 var connection = mysql.createConnection({
@@ -12,6 +14,25 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if(err) throw err;
-    console.log("Connected");
+    inStock();
     connection.end();
 });
+
+function inStock() {
+    connection.query("SELECT * FROM products",
+    function(err, res) {
+        if(err) throw err;
+        var table = new Table({
+            head: ["Product Number".bold.brightYellow, "Product Name".bold.brightYellow, "Department".bold.brightYellow, "Price".bold.brightYellow, "Amount in Stock".bold.brightYellow],
+            colWidths: [20, 20, 20, 20, 20]
+        });
+
+        console.log("----- Products in Stock -----".bold.brightYellow);
+        for(var i = 0; i < res.length; i++){
+            table.push(
+                [res[i].sku, res[i].product_name, res[i].department, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(table.toString());
+    });
+};
